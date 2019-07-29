973bf1b14555635f9a6ea371ad805b2f6a43d624
Guy Leonard <guy.leonard@gmail.com>
2019-07-29 18:01:27 +0100

    normal

--- cider_db/create_cider_db.sh
+++ cider_db/create_cider_db.sh
@@ -20,17 +20,17 @@ echo "[CREATE] Creating orchardDB with ${USER}:${PASS}@${DB}"
 #
 # Archaeplastida
 #
-#echo "[CREATE] Downloading Archaeplastida Genomes"
-#./get_archaeplastida_genomes.sh
-#echo -e "\tMove Original FASTA Records"
-#mkdir -p ${DB}/archaeplastida
-#echo "[CREATE] UnGzip Files"
-#pigz -d *.gz
-#mv *.aa *.fa *.fna *.faa *.fasta *.pep *.protein *.tfa ${DB}/archaeplastida
-#echo "[CREATE] Archaeplastida Genomes"
-#./insert_archaeplastida_genomes.sh ${USER} ${PASS} ${DB}
-#echo -e "\tGZIP Original FASTA Records"
-#pigz -9 -R ${DB}/archaeplastida/* 
+echo "[CREATE] Downloading Archaeplastida Genomes"
+./get_archaeplastida_genomes.sh
+echo -e "\tMove Original FASTA Records"
+mkdir -p ${DB}/archaeplastida
+echo "[CREATE] UnGzip Files"
+pigz -d *.gz
+mv *.aa *.fa *.fna *.faa *.fasta *.pep *.protein *.tfa ${DB}/archaeplastida
+echo "[CREATE] Archaeplastida Genomes"
+./insert_archaeplastida_genomes.sh ${USER} ${PASS} ${DB}
+echo -e "\tGZIP Original FASTA Records"
+pigz -9 -R ${DB}/archaeplastida/* 
 
 #
 # Metazoa
