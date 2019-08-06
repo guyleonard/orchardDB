85b3cf8b03e9518f301a5ec3632e5389357515ff
Guy Leonard <guy.leonard@gmail.com>
2019-08-06 16:34:45 +0100

    neater output

--- cider_db/create_cider_db.sh
+++ cider_db/create_cider_db.sh
@@ -7,70 +7,70 @@ DB=$3
 NUMARGS=$#
 
 if [ "${NUMARGS}" -eq 0 ]; then
-    echo "You are missing paramaters..."
+    echo "[CREATE:ERRR] - You are missing paramaters..."
     exit 1
 fi
 
 if [[ ! -f nodes.dmp || ! -f names.dmp ]]; then
-	echo "The NCBI Taxdmp files are missing!";
+	echo "[CREATE:WARN] - The NCBI Taxdmp files are missing!";
 	wget -nc ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
 	tar zxvf taxdump.tar.gz names.dmp nodes.dmp
 	rm taxdump.tar.gz
 fi
 
 if [ ! -f taxonomy.sqlite ]; then
-	echo "taxonomy.sqlite is Missing. It will be generated on the first pass.";
+	echo "[CREATE:WARN] - taxonomy.sqlite is Missing. It will be generated on the first pass.";
 fi
 
 #
 # Setup
 #
-echo "[CREATE] Creating orchardDB with ${USER}:${PASS}@${DB}"
+echo "[CREATE:INFO] - Creating orchardDB with ${USER}:${PASS}@${DB}"
 ../bin/plant --setup --user ${USER} --pass ${PASS} --db ${DB}
 
 #
 # Archaeplastida
 #
-echo "[CREATE] Downloading Archaeplastida Genomes"
+echo "[CREATE:INFO] - Downloading Archaeplastida Genomes"
 ./get_archaeplastida_genomes.sh
-echo -e "\tMove Original FASTA Records"
 mkdir -p ${DB}/archaeplastida
-echo "[CREATE] UnGzip Files"
-pigz -d *.gz
+echo "[CREATE:INFO] - UnGzip Files"
+gunzip *.gz
+echo "[CREATE:INFO] - Move Original FASTA Records"
 mv *.aa *.fa *.fna *.faa *.fasta *.pep *.protein *.tfa ${DB}/archaeplastida
-echo "[CREATE] Archaeplastida Genomes"
+echo "[CREATE:INFO] - Insert Archaeplastida Genomes"
 ./insert_archaeplastida_genomes.sh ${USER} ${PASS} ${DB}
-echo -e "\tGZIP Original Archaeplastida FASTA Records"
+echo -e "[CREATE:INFO] - GZIP Original Archaeplastida FASTA Records"
 pigz -9 -R ${DB}/archaeplastida/*
 
 #
 # Metazoa
 #
-echo "[CREATE] Downloading Metazoan Genomes"
+echo "[CREATE:INFO] - Downloading metazoa Genomes"
 ./get_metazoan_genomes.sh
-echo -e "\tMove Original FASTA Records"
 mkdir -p ${DB}/metazoa
-echo "[CREATE] UnGzip Files"
-pigz -d *.gz
-mv *.aa *.fa *.fna *.faa *.fasta *.pep *.protein *.prot *.tfa ${DB}/metazoa
-echo "[CREATE] Metazoa Genomes"
+echo "[CREATE:INFO] - UnGzip Files"
+gunzip *.gz
+echo "[CREATE:INFO] - Move Original FASTA Records"
+mv *.aa *.fa *.fna *.faa *.fasta *.pep *.protein *.tfa ${DB}/metazoa
+echo "[CREATE:INFO] - Insert metazoa Genomes"
 ./insert_metazoan_genomes.sh ${USER} ${PASS} ${DB}
-echo -e "\tGZIP Original Metazoan FASTA Records"
+echo -e "[CREATE:INFO] - GZIP Original metazoa FASTA Records"
 pigz -9 -R ${DB}/metazoa/*
 
 #
 # Protists
 #
-echo "[CREATE] Downloading Protist Genomes"
+echo "[CREATE:INFO] - Downloading protist Genomes"
 ./get_protist_genomes.sh
-echo -e "\tMove Original FASTA Records"
 mkdir -p ${DB}/protists
-echo "[CREATE] UnGzip Files"
-pigz -d *.gz
-mv *.aa *.fa *.fna *.faa *.fasta *.pep *.protein *.prot *.tfa ${DB}/protists
-echo "[CREATE] Protist Genomes"
+echo "[CREATE:INFO] - UnGzip Files"
+gunzip *.gz
+echo "[CREATE:INFO] - Move Original FASTA Records"
+mv *.aa *.fa *.fna *.faa *.fasta *.pep *.protein *.tfa ${DB}/protists
+echo "[CREATE:INFO] - Insert protist Genomes"
 ./insert_protist_genomes.sh ${USER} ${PASS} ${DB}
-echo -e "\tGZIP Original Protist FASTA Records"
-pigz -9 -R ${DB}/protists/* 
+echo -e "[CREATE:INFO] - GZIP Original protist FASTA Records"
+pigz -9 -R ${DB}/protists/*
 
 exit 0
